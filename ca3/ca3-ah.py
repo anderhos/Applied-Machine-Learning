0a954f912b89fb614c32faeaec079d9aa4767bf0
Anders Høst <andersm_host@hotmail.com>
2020-07-20 13:59:29 +0200

    add files

---
+++ ca3/ca3-ah.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Mølmen Høst'
+__email__ = 'anders.molmen.host@nmbu.no'
+
+"""
+Compulsory assignment 3
+"""
+
+"""
+- Read the data
+- Split dataset into training and test datasets
+- Standardize (necessary for all but decision trees/random forest?)
+- 
+"""
+# Start by importing the data
