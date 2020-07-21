51a274632c8866f6585f172c4084f8a3f138a137
Anders Høst <andersm_host@hotmail.com>
2020-07-21 15:24:48 +0200

    fit perceptron

--- ca3/ca3-ah.py
+++ ca3/ca3-ah.py
@@ -19,10 +19,11 @@ Compulsory assignment 3
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 # Read data
 df = pd.read_csv('CA3-train.csv')
@@ -47,21 +48,36 @@ pca = PCA(n_components=None)
 # Dimensionality reduction
 X_train_pca = pca.fit_transform(X_train_std)
 X_test_pca = pca.fit_transform(X_test_std)
-print(pca.explained_variance_ratio_)
+#print(pca.explained_variance_ratio_)
 
 # plot cumulative sum of explained variances
 var_exp = pca.explained_variance_ratio_
 cum_var_exp = np.cumsum(var_exp)
+#print(np.cumsum(var_exp)[9])
 plt.bar(range(1, 25), var_exp, alpha=0.5, align='center', label='Individual explained variances')
 plt.step(range(1, 25), cum_var_exp, where='mid', label='Cumulative explained variances')
 plt.xlabel('Explained variance ratio')
 plt.ylabel('Principal component index')
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+#plt.show()
+
+# Fitting the Perceptron on the reduced dataset
+ppn = Perceptron(eta0=0.1, random_state=1)
+ppn.fit(X_train_pca, y_train)
+y_pred = ppn.predict(X_test_pca)
+print(f'Misclassified examples: {(y_test != y_pred).sum()}')
+print('Accuracy: {:.3}'.format(ppn.score(X_test_pca, y_test)))
+
+# Fitting the Perceptron on the original dataset
+ppn2 = Perceptron(eta0=0.1, random_state=1)
+ppn2.fit(X_train_std, y_train)
+y_pred = ppn2.predict(X_test_std)
+print(f'Misclassified examples: {(y_test != y_pred).sum()}')
+print('Accuracy: {:.3}'.format(ppn2.score(X_test, y_test)))
+
+
+
 
 # Fitting the logistic regression model on the reduced dataset
 #lr.fit(X_train_pca, y_train)
-
-
-
